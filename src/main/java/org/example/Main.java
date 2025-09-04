d2815d475271b72014f71575dc32bd1ff2a00f0d
jamuna112 <jamunaadhikari112@gmail.com>
2025-09-04 09:44:45 -0700

    linked list basic functionality

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,27 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.linkedlist.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World..");
+        Cookie cookieOne = new Cookie("Green");
+        Cookie cookieTwo = new Cookie("Red");
+
+        cookieOne.setColor("Yellow");
+
+        System.out.println(cookieOne.getColor());
+        System.out.println(cookieTwo.getColor());
+
+        LinkedList ll = new LinkedList(7);
+
+        ll.append(6);
+        ll.append(5);
+        ll.getHead();
+        ll.getTail();
+        ll.getLength();
+        ll.printList();
+
+
 
     }
 }
---
+++ src/main/java/org/example/linkedlist/LinkedList.java
@@ -0,0 +1,59 @@
+package org.example.linkedlist;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node node = new Node(value);
+        head = node;
+        tail = node;
+        length = 1;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public void printList() {
+        Node temp = head;
+
+        while(temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public void getHead(){
+        System.out.println("Head: "+ head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: "+ tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: "+ length);
+    }
+}
