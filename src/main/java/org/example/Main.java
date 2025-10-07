0279853e12e5ab268c074ee443de869295c4b38c
Jamuna <jamunaadhikari112@gmail.com>
2025-10-07 12:10:35 -0700

    remove edge and vertex methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -118,8 +118,17 @@ public class Main {
         Graph myGraph = new Graph();
         myGraph.addVertex("A");
         myGraph.addVertex("B");
+        myGraph.addVertex("C");
+        myGraph.addVertex("D");
 
         myGraph.addEdge("A", "B");
+        myGraph.addEdge("A", "C");
+        myGraph.addEdge("A", "D");
+        myGraph.addEdge("B", "D");
+        myGraph.addEdge("C", "D");
+
+        //myGraph.removeEdge("A", "B");
+        myGraph.removeVertex("D");
         myGraph.printGraph();
 
     }
