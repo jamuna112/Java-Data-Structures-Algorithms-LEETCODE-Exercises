e6fa70dde35d6a57fd3c99f25d63441c29714fc0
Jamuna <jamunaadhikari112@gmail.com>
2025-10-08 09:37:49 -0700

    heap setup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -115,21 +115,22 @@ public class Main {
 //        System.out.println("longest sequence number is "+longestSequence);
 
         // ======= Graph =======
-        Graph myGraph = new Graph();
-        myGraph.addVertex("A");
-        myGraph.addVertex("B");
-        myGraph.addVertex("C");
-        myGraph.addVertex("D");
+//        Graph myGraph = new Graph();
+//        myGraph.addVertex("A");
+//        myGraph.addVertex("B");
+//        myGraph.addVertex("C");
+//        myGraph.addVertex("D");
+//
+//        myGraph.addEdge("A", "B");
+//        myGraph.addEdge("A", "C");
+//        myGraph.addEdge("A", "D");
+//        myGraph.addEdge("B", "D");
+//        myGraph.addEdge("C", "D");
+//
+//        //myGraph.removeEdge("A", "B");
+//        myGraph.removeVertex("D");
+//        myGraph.printGraph();
 
-        myGraph.addEdge("A", "B");
-        myGraph.addEdge("A", "C");
-        myGraph.addEdge("A", "D");
-        myGraph.addEdge("B", "D");
-        myGraph.addEdge("C", "D");
-
-        //myGraph.removeEdge("A", "B");
-        myGraph.removeVertex("D");
-        myGraph.printGraph();
 
     }
 }
